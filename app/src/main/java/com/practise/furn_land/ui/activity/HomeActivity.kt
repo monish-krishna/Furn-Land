1e8587573921342d2205b0584f5ad5419db61549
monish-krishna <monishkrishnas47@gmail.com>
2022-02-09 11:01:50 +0530

    Removed unnecessary usage.

--- app/src/main/java/com/practise/furn_land/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/practise/furn_land/ui/activity/HomeActivity.kt
@@ -64,10 +64,7 @@ class HomeActivity : AppCompatActivity() {
                 R.id.homeFragment -> showBottomNavBar()
                 R.id.searchFragment -> showBottomNavBar()
                 R.id.profileFragment -> showBottomNavBar()
-                R.id.productListFragment ->{
-                    hideBottomNavBar()
-                    invalidateOptionsMenu()
-                }
+                R.id.productListFragment -> hideBottomNavBar()
                 R.id.newAddressDialogFragment-> hideBottomNavBar()
                 else -> hideBottomNavBar()
             }
