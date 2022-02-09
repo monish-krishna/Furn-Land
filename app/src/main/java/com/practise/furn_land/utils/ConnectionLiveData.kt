30337abf38b896726cfb20eae020ae55491fce2e
monish-krishna <monishkrishnas47@gmail.com>
2022-02-09 13:54:10 +0530

    Class for providing liveData of connection availability.

---
+++ app/src/main/java/com/practise/furn_land/utils/ConnectionLiveData.kt
@@ -0,0 +1,87 @@
+package com.practise.furn_land.utils
+
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkRequest
+import android.util.Log
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.net.InetSocketAddress
+import javax.net.SocketFactory
+
+class ConnectionLiveData(context: Context): LiveData<Boolean>(true) {
+
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
+    private val cm = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val validNetworks: MutableSet<Network> = HashSet()
+
+    override fun onActive() {
+        super.onActive()
+        networkCallback = getNetworkCallback()
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NET_CAPABILITY_INTERNET)
+            .build()
+        cm.registerNetworkCallback(networkRequest,networkCallback)
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        cm.unregisterNetworkCallback(networkCallback)
+    }
+
+    private fun getNetworkCallback() = object : ConnectivityManager.NetworkCallback(){
+
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+
+            val networkCapabilities = cm.getNetworkCapabilities(network)
+            val hasInternetCapability = networkCapabilities?.hasCapability(NET_CAPABILITY_INTERNET)
+
+            if (hasInternetCapability == true){
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    val hasInternet = doesNetworkActuallyHasInternet(network.socketFactory)
+                    if(hasInternet){
+                        withContext(Dispatchers.Main){
+                            validNetworks.add(network)
+                            checkValidNetworks()
+                        }
+                    }
+                }
+            }
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            validNetworks.remove(network)
+            checkValidNetworks()
+        }
+    }
+
+    private fun checkValidNetworks() {
+        postValue(validNetworks.size> 0)
+    }
+
+    private fun doesNetworkActuallyHasInternet(socketFactory: SocketFactory): Boolean{
+        return try{
+            val socket = socketFactory.createSocket() ?: throw IOException("Socket is null.")
+            socket.connect(InetSocketAddress("8.8.8.8", 53), 1500)
+            socket.close()
+            true
+        }catch (e: IOException){
+            Log.e(TAG, "No internet connection. ${e}")
+            false
+        }
+    }
+
+    companion object{
+        private const val TAG = "ConnectionLiveData"
+    }
+}
